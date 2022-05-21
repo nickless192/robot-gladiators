4e3dbd4c2aac29909e690006939d5a7be05d651b
Omar Rodriguez <omar.rguez26@gmail.com>
2022-05-21 12:36:03 -0400

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -31,7 +31,7 @@ var fight = function(enemyName) {
             if (confirmSkip) {
                 window.alert( playerName + " has chosen to skip the fight. Goodbye!");
                 // subtract money from playerMoney for skipping
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log("playerMoney ", playerMoney);
                 break;
             }
@@ -42,7 +42,8 @@ var fight = function(enemyName) {
 
 
         //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to updat the value in the `enemyHealth` variable
-        enemyHealth = enemyHealth - playerAttack;
+        var damage = randomNumber(playerAttack - 3, playerAttack);
+        enemyHealth = Math.max(0, enemyHealth - damage);
 
         // Log a resulting message to the console so we know that it worked
         console.log(
@@ -59,7 +60,8 @@ var fight = function(enemyName) {
         };
 
         // Subtract the value of the `enemyAtack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable
-            playerHealth = playerHealth - enemyAttack;
+        var damage = randomNumber(enemyAttack - 3, enemyAttack);
+        playerHealth = Math.max(0, playerHealth - damage);
 
         // Log a resulting message to the console so we know it worked
         console.log(
@@ -89,7 +91,7 @@ var startGame = function() {
         if (playerHealth > 0) {
             window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
             var pickedEnemyName = enemyNames[i];
-            enemyHealth = 50;
+            enemyHealth = randomNumber(40, 60);
             fight(pickedEnemyName);
             // if we're not at the last enemy in the array
             if (playerHealth > 0 && i < enemyNames.length - 1) {
@@ -171,6 +173,12 @@ var shop = function() {
                 break;
     }
 };
+
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    return value;
+}
 
 // start the game when the page loads
 startGame();
